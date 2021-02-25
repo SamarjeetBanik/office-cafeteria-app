1e70a69d4fc6a0e5e6164cd9e17ac138c7b9fc6a
Samarjeet <samarjeetbanik@gmail.com>
2021-02-25 21:03:00 +0530

    updated app.js

--- projbackend/app.js
+++ projbackend/app.js
@@ -8,6 +8,10 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
 const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
 
 // DB Connection
 mongoose.connect(process.env.DATABASE, {
@@ -24,7 +28,11 @@ app.use(cookieParser());
 app.use(cors());
 
 // Routes
-app.use("/api", authRoutes);
+app.use("/cafe", authRoutes);
+app.use("/cafe", userRoutes);
+app.use("/cafe", categoryRoutes);
+app.use("/cafe", productRoutes);
+app.use("/cafe", orderRoutes);
 
 // PORT
 const port = process.env.PORT || 3300;
